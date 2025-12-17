8bf31f33a21843b3e8f08b3b577359166dae85f4
arthurovale <34468091+arthurovale@users.noreply.github.com>
2025-12-17 17:19:21 -0300

    More steps towards Karoubi

--- interfaces/DoubleKaroubi.v
+++ interfaces/DoubleKaroubi.v
@@ -2,7 +2,7 @@ Require Import interfaces.Category.
 Require Import interfaces.Functor.
 Require Import interfaces.DoubleCat.
 
-Module Idempotents (V : CategoryDefinition) (D : DoubleCategory V).
+Module Idempotents (V : CategoryDefinition) (D : DoubleCategory V) <: Category.
   Import D.
   Import Siso.
 
@@ -12,6 +12,15 @@ Module Idempotents (V : CategoryDefinition) (D : DoubleCategory V).
       idempotency : sisocell (carrier ⨀ carrier) carrier;
       idem_coh : (idempotency ⊙ vid carrier) ;; idempotency =
         assoc carrier carrier carrier ;; (vid carrier ⊙ idempotency) ;; idempotency;
+    }.
+  Arguments carrier {_}.
+  Arguments idempotency {_}.
+  Arguments idem_coh {_}.    
+
+  Record idem_obj := 
+    mk_idem_obj {
+      idem_type : V.t;
+      idem_carrier :> idem idem_type
     }.
 
   Program Definition id_idem (a : V.t) :=
@@ -23,5 +32,34 @@ Module Idempotents (V : CategoryDefinition) (D : DoubleCategory V).
     rewrite <- lunit_hid_runit_hid. rewrite <- unit_coh.
     rewrite lunit_hid_runit_hid. reflexivity.
   Defined.
+
+  Definition idem_to_obj {a : V.t} : idem a -> idem_obj 
+    := fun e => mk_idem_obj a e.
+  Coercion idem_to_obj : idem >-> idem_obj.
+  
+  Definition t : Type := idem_obj.
+
+  Record idem_vmor {a : V.t} (e : idem a) {b : V.t} (e' : idem b) :=
+    {
+      idem_vmor_carrier :> V.m a b;
+      idem_vmor_sq : e =[idem_vmor_carrier, idem_vmor_carrier]=> e';
+      idem_vmor_coh : idempotency e ;; idem_vmor_sq = 
+        idem_vmor_sq ⊙ idem_vmor_sq ;; idempotency e';
+    }.
+
+  Definition m : t -> t -> Type := fun e => fun e' => idem_vmor e e'.
+
+  Program Definition idem_id_vmor {a : V.t} (e : idem a) : idem_vmor e e :=
+    {|
+      idem_vmor_carrier := V.id a;
+      idem_vmor_sq := vid e;
+    |}.
+  Next Obligation.
+    rewrite hcomp_fmap_id. rewrite vid_is_Vert_id.
+    rewrite Vert.compose_id_left. rewrite vid_is_Vert_id. 
+    rewrite Vert.compose_id_right. reflexivity.
+  Defined.
+
+  Definition id (e : t) : m e e := idem_id_vmor e.
 
 End Idempotents.
