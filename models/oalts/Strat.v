bf3f6f5ea9c70c13460a03a8574f1de76dfb9591
arthurovale <34468091+arthurovale@users.noreply.github.com>
2025-12-26 22:50:29 -0300

    Id strategy and stateless functor mapping

--- models/oalts/Strat.v
+++ models/oalts/Strat.v
@@ -3,6 +3,8 @@ Require Import oalts.AsyncEvents.
 Require Import oalts.Sig.
 Require Import oalts.Tree.
 
+From Paco Require Import paco.
+
 Module Strat. (* <: Category *)
   Import AsyncEvents.
   Import Sig.
@@ -13,11 +15,41 @@ Module Strat. (* <: Category *)
 
   Definition id_gen {A : sig} (ev : [A]) : «A -o A» :=
     match ev with
-    | ⊖ a => ⟨⊕a | ⊖a⟩
-    | ⊕ a => ⟨⊖a | ⊕a⟩
+    | inl a => ⟨tgt a | src a⟩
+    | inr a => ⟨src a | tgt a⟩
     end.
 
   CoFixpoint id (A : sig) : strat A A :=
     go (StepF (X := [A]) id_gen (fun _ => id A)).
 
+  Module StLess.
+    Import Sets.
+
+    CoFixpoint StLess {A B : sig} (gen : Sig.m A B) : strat A B :=
+      go (StepF (X := [A]) 
+        (fun ev => 
+          match ev with 
+          | inl a => 
+            match gen^- a with 
+            | 'b => ⟨tgt b | src a⟩
+            | ɛ => ⟨ | src a⟩
+            end
+          | inr a =>
+            match gen^+ a with 
+            | 'b => ⟨src a | tgt b⟩
+            | ɛ => ⟨src a | ⟩
+            end
+          end)
+        (fun _ => StLess gen)).
+
+    Proposition id_stless_id {A : sig} :
+      bisim (id A) (StLess (Sig.id A)).
+    Proof.
+      pcofix IH. pfold. unfold bisimF. simpl.
+      split; intros ev; exists ev;
+      destruct ev as [a | a]; split; 
+      try reflexivity; right; exact IH.
+    Qed.
+  End StLess.
+    
 End Strat.
